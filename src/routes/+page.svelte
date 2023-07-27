d18556802b6df95c02dc051035993d41b4ed70d1
Kawsar Ahmed <prokawsar@gmail.com>
2023-07-27 15:45:07 +0600

    index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+</script>
+
+<svelte:head>
+	<title>Dashboard starter</title>
+</svelte:head>
+
+<h1 class="text-3xl font-bold underline mx-auto container flex justify-center">Hello world!</h1>
