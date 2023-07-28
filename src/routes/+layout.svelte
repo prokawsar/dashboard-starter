508ccf1b7f994ccbcf29bfcc25b3811623c33b73
Kawsar Ahmed <prokawsar@gmail.com>
2023-07-28 11:14:30 +0600

    posthog added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+	import posthog from 'posthog-js';
+
+	$: $page.url.pathname, browser && posthog.capture('$pageview', { layout: true });
 </script>
 
 <slot />
