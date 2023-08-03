75e72e38cc7ebc8a94b71ff4e7918c260fd9698f
Kawsar Ahmed <prokawsar@gmail.com>
2023-08-03 09:39:59 +0600

    tooltip ex

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,9 @@
 	<title>Dashboard starter</title>
 </svelte:head>
 
-<h1 class="text-3xl font-bold underline mx-auto container flex justify-center">Hello world!</h1>
+<h1
+	data-tippy="Hello Tooltip"
+	class="text-3xl font-bold underline mx-auto container flex justify-center"
+>
+	Hello world!
+</h1>
