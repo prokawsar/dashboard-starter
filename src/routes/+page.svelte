57b3f812c18ba4a35bf7419f12b6e5df6ee4c3ac
Kawsar Ahmed <prokawsar@gmail.com>
2023-12-27 11:02:53 +0600

    migration to sveltekit 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { i, languages, switchLanguage } from '@inlang/sdk-js';
+	// import { i, languages, switchLanguage } from '@inlang/sdk-js';
 </script>
 
 <svelte:head>
@@ -7,14 +7,14 @@
 </svelte:head>
 
 <div class="flex flex-row gap-2 container mx-auto justify-center">
-	{#each languages as lang}
+	<!-- {#each languages as lang}
 		<button on:click={() => switchLanguage(lang)}>{lang}</button>
-	{/each}
+	{/each} -->
 </div>
 
 <h1
 	data-tippy="Hello Tooltip"
 	class="text-3xl mt-10 font-bold underline mx-auto container flex justify-center"
 >
-	{i('welcome')}
+	<!-- {i('welcome')} -->
 </h1>
