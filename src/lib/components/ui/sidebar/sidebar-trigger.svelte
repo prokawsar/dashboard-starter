97211756a5d69c4087c521646549a62d1ce8ccdd
Kawsar Ahmed <prokawsar@gmail.com>
2025-01-06 09:04:11 +0600

    Refactor

--- src/lib/components/ui/sidebar/sidebar-trigger.svelte
+++ src/lib/components/ui/sidebar/sidebar-trigger.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { Button } from "$lib/components/ui/button/index.js";
-	import { cn } from "$lib/utils.js";
-	import PanelLeft from "lucide-svelte/icons/panel-left";
-	import type { ComponentProps } from "svelte";
-	import { useSidebar } from "./context.svelte.js";
+	import { Button } from '$lib/components/ui/button/index.js'
+	import { cn } from '$lib/utils.js'
+	import PanelLeft from 'lucide-svelte/icons/panel-left'
+	import type { ComponentProps } from 'svelte'
+	import { useSidebar } from './context.svelte.js'
 
 	let {
 		ref = $bindable(null),
@@ -11,24 +11,24 @@
 		onclick,
 		...restProps
 	}: ComponentProps<typeof Button> & {
-		onclick?: (e: MouseEvent) => void;
-	} = $props();
+		onclick?: (e: MouseEvent) => void
+	} = $props()
 
-	const sidebar = useSidebar();
+	const sidebar = useSidebar()
 </script>
 
 <Button
 	type="button"
 	onclick={(e) => {
-		onclick?.(e);
-		sidebar.toggle();
+		onclick?.(e)
+		sidebar.toggle()
 	}}
 	data-sidebar="trigger"
 	variant="ghost"
 	size="icon"
-	class={cn("h-7 w-7", className)}
+	class={cn('h-7 w-7', className)}
 	{...restProps}
 >
-	<PanelLeft />
+	<PanelLeft class="!w-6 !h-6" />
 	<span class="sr-only">Toggle Sidebar</span>
 </Button>
--- src/routes/(app)/dashboard/+layout.svelte
+++ src/routes/(app)/dashboard/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import AppSidebar from '$lib/components/app-sidebar.svelte';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import '@/app.css';
-	import { setContext } from 'svelte';
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import "@/app.css";
+	import { setContext } from "svelte";
 
 	let { children, data } = $props();
 
-	setContext('user', data.user);
+	setContext("user", data.user);
 </script>
 
 <Sidebar.Provider>
