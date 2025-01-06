1729d8016778a74b131634c901d70e485f87256a
Kawsar Ahmed <prokawsar@gmail.com>
2025-01-06 10:02:55 +0600

    Fix layout

--- src/routes/(app)/dashboard/+layout.svelte
+++ src/routes/(app)/dashboard/+layout.svelte
@@ -11,10 +11,19 @@
 
 <Sidebar.Provider>
 	<AppSidebar />
-	<Sidebar.Inset class="!h-full w-full">
-		<main class="h-full w-full p-4">
-			<Sidebar.Trigger />
-			{@render children?.()}
-		</main>
+	<Sidebar.Inset class="!h-full w-full p-4">
+		<header
+			class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+		>
+			<div class="flex w-full flex-row items-center justify-between">
+				<Sidebar.Trigger class="" />
+				<div class="flex flex-row gap-3 pr-4">
+					<h1 class="text-lg">Dashboard</h1>
+					<h1 class="text-lg">Account</h1>
+				</div>
+			</div>
+		</header>
+
+		{@render children?.()}
 	</Sidebar.Inset>
 </Sidebar.Provider>
